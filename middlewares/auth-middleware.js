bb5f2adce9c68e29090078f32edacb1370d9ddba
wiphaphim <wipha.ph@inet.co.th>
2020-11-17 15:28:30 +0700

    add descripts in middleware

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
             .get("Cookie")
             .spilt("token=")[1]
             .trim()
-        jwt.verify(token, "SECRETKEY")  //* 
+        jwt.verify(token, "SECRETKEY")  //* check token ถอดรหัส token ด้วย SECRETKEY
         next()
     } catch (error) {
         res.status(401).render("login", {
